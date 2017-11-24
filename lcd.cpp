1c8d2ad536ae5346522e99a48e9704a6e6eb5ded
Danny Glover <dcgm1986@gmail.com>
2017-11-24 00:49:42 +0000

    Fix initial screen drawing
    - Make sure to draw at least one frame on startup.

--- lcd.cpp
+++ lcd.cpp
@@ -34,6 +34,9 @@ void Lcd::Init()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glEnable(GL_TEXTURE_2D);
+
+	// draw one frame
+	DrawScreen();
 }
 
 // reset the lcd
