40ed8c50f60e50f4376b6f5d8fd9aabb038f1384
Danny Glover <dcgm1986@gmail.com>
2017-11-24 03:28:05 +0000

    Adding stub MBC1 class

---
+++ include/mbc1.h
@@ -0,0 +1,22 @@
+/*
+	Project: cBoy: A Gameboy emulator written in C++
+	Author: Danny Glover - https://github.com/DannyGlover
+	File: mbc1.h
+*/
+
+#ifndef MBC1_H
+#define MBC1_H
+
+// includes
+#include "typedefs.h"
+
+// definitions
+
+// mbc1 class
+class Mbc1
+{
+	public:
+		// TODO
+};
+
+#endif
---
+++ mbc1.cpp
@@ -0,0 +1,15 @@
+/*
+	Project: cBoy: A Gameboy emulator written in C++
+	Author: Danny Glover - https://github.com/DannyGlover
+	File: mbc1.cpp
+*/
+
+// includes
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+#include "include/memory.h"
+#include "include/mbc1.h"
+#include "include/log.h"
+
+// TODO
